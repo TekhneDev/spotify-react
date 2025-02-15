a1b5ea5e278fb4e6c2416c39b55e47bc420886c1
Carla Mariana <carlamab2808@outlook.com>
2025-02-15 01:33:49 -0300

    update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,26 +2,34 @@ import ItemList from "./ItemList";
 import { artistArray } from "../assets/database/artists";
 import { songsArray } from "../assets/database/songs";
 
-const Main = ({type}) => {
+const Main = ({ type }) => {
   return (
     <div className="main">
       {/* Item List Artirts */}
-      <ItemList
-        title="Artistas"
-        items={10}
-        itemsArray={artistArray}
-        path="/artists"
-        idPath="/artist"
-      ></ItemList>
+      {type === "artists" || type === undefined ? (
+        <ItemList
+          title="Artistas"
+          items={10}
+          itemsArray={artistArray}
+          path="/artists"
+          idPath="/artist"
+        ></ItemList>
+      ) : (
+        <></>
+      )}
 
       {/* Item List Songs */}
-      <ItemList
-        title="Músicas"
-        items={20}
-        itemsArray={songsArray}
-        path="/songs"
-        idPath="/song"
-      ></ItemList>
+      {type === "songs" || type === undefined ? (
+        <ItemList
+          title="Músicas"
+          items={20}
+          itemsArray={songsArray}
+          path="/songs"
+          idPath="/song"
+        ></ItemList>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
