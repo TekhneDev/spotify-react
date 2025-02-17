8aedcedb0ccf476be2c485d5ad9e286c35ad0d8d
Carla Mariana <carlamab2808@outlook.com>
2025-02-16 22:44:08 -0300

    update artist

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -1,5 +1,22 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
 const Artist = () => {
-  return <div>Artist</div>;
+  return (
+    <div className="artist">
+      <div className="artist__header"></div>
+
+      <div className="artist__body"></div>
+
+      <Link to="/song/1">
+        <FontAwesomeIcon
+          className="single-item__icon--artist"
+          icon={faCirclePlay}
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default Artist;
