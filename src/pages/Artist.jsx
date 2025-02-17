a47825b101db9c893c01dd595e2c957bb1992d06
Carla Mariana <carlamab2808@outlook.com>
2025-02-16 22:57:42 -0300

    .

--- src/pages/Artist.jsx
+++ src/pages/Artist.jsx
@@ -5,7 +5,15 @@ import { Link } from "react-router-dom";
 const Artist = () => {
   return (
     <div className="artist">
-      <div className="artist__header"></div>
+      <div
+        className="artist__header"
+        style={{
+          backgroundImage:
+            "linear-gradient(to bottom, var(--_shade), var(--_shade)), url(https://i.scdn.co/image/ab67618600001016e2952bec9cfd4e6862e23607)",
+        }}
+      >
+        <h2 className="artist_title">Jorge & Mateus</h2>
+      </div>
 
       <div className="artist__body"></div>
 
