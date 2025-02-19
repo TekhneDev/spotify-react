d2de02d8d9ccb4fd1916109e08e065d3dc0d31f5
Carla Mariana <carlamab2808@outlook.com>
2025-02-18 23:46:00 -0300

    update css

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -5,9 +5,9 @@ const SongItem = () => {
     <Link to="/song/1" className="song-item">
       <p>1</p>
 
-      <img src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24" alt="Imagem da Música X"></img>
+      <img src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24" alt="Imagem da Música X" className="song-item__image"></img>
 
-      <p>Nome Música</p>
+      <p className="song-item__name">Nome Música</p>
 
       <p>02:30</p>
     </Link>
