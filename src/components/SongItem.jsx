240cd14ed0e6853493015ed38a5fb1b8c96dfc96
Carla Mariana <carlamab2808@outlook.com>
2025-02-18 23:57:55 -0300

    update

--- src/components/SongItem.jsx
+++ src/components/SongItem.jsx
@@ -3,11 +3,19 @@ import { Link } from "react-router-dom";
 const SongItem = () => {
   return (
     <Link to="/song/1" className="song-item">
-      <p>1</p>
+      <div className="song-item__number-album">
+        <p>1</p>
 
-      <img src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24" alt="Imagem da Música X" className="song-item__image"></img>
+        <div className="song-item__album">
+          <img
+            src="https://i.scdn.co/image/ab67616d00001e022774b00531d558bc19e12a24"
+            alt="Imagem da Música X"
+            className="song-item__image"
+          ></img>
 
-      <p className="song-item__name">Nome Música</p>
+          <p className="song-item__name">Nome Música</p>
+        </div>
+      </div>
 
       <p>02:30</p>
     </Link>
