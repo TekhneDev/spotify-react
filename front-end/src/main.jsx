51b27a7ebfe205bc3813ca52801a6e4c60c0cfcb
Carla Mariana <carlamab2808@outlook.com>
2025-02-22 23:49:16 -0300

    .

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx"; // default
+import React from "react";
+import ItemList from "./ItemList";
+import { artistArray } from "../assets/database/artists";
+import { songsArray } from "../assets/database/songs";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const Main = ({ type }) => {
+  return (
+    <div className="main">
+      {/* Item List de Artistas */}
+      {type === "artists" || type === undefined ? (
+        <ItemList
+          title="Artistas"
+          items={10}
+          itemsArray={artistArray}
+          path="/artists"
+          idPath="/artist"
+        />
+      ) : (
+        <></>
+      )}
+
+      {/* Item List de Músicas */}
+      {type === "songs" || type === undefined ? (
+        <ItemList
+          title="Músicas"
+          items={20}
+          itemsArray={songsArray}
+          path="/songs"
+          idPath="/song"
+        />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
+
+export default Main;
